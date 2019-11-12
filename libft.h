0158bfd3fabb62296f53802cad8082d6c8e06a2f
Dumont TONY <dtony@e2r10p17.42.fr>
2019-11-12 17:11:11 +0100

    func + strchr to do

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dtony <dtony@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 16:22:49 by dtony             #+#    #+#             */
-/*   Updated: 2019/11/11 16:54:52 by dtony            ###   ########.fr       */
+/*   Updated: 2019/11/12 17:06:50 by dtony            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,3 +20,10 @@ void    *ft_memmove(void *dst, const void *src, size_t len);
 void    *ft_memchr(const void *s, int c, size_t n);
 int     ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t  ft_strlen(const char *s);
+int     ft_isalpha(int c);
+int     ft_isdigit(int c);
+int     ft_isalnum(int c);
+int     ft_isascii(int c);
+int     ft_isprint(int c);
+int     ft_toupper(int c);
+int     ft_tolower(int c);
